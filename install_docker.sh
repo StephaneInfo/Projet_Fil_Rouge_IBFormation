d87db3290e8ada625c29f7170795f5cb514a9453
TECHER Stephane <74711672+StephaneInfo@users.noreply.github.com>
2022-05-05 17:08:22 +0200

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum -y update
+
+# install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker vagrant
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# install docker compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
